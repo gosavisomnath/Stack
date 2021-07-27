cfaf70018a3fc33ad9c24b1bfb5e6f0c8ec52494
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-27 14:12:30 +0530

    Created Stack

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,5 +1,21 @@
 public class MyStack {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the stack program");
+    MyLinkedList myLinkedList= new MyLinkedList();
+    private int default_size=3;
+
+    public MyStack(MyLinkedList myLinkedList) {
+        this.myLinkedList = myLinkedList;
+    }
+
+    public MyStack(int size) {
+        this.myLinkedList = myLinkedList;
+        this.default_size = default_size;
+    }
+    public void push(INode node){
+        myLinkedList.add(node);
+
+    }
+    public void printStack()
+    {
+        myLinkedList.printNodes();
     }
 }
